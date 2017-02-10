6833a24412f47572fe6b6e2318c4d892cedcc4c0
Venkat Polumahanti <venkat4541@gmail.com>
2017-02-09 21:37:18 -0500

    Route setup for the blog
    Routes have been created for the basic structure of the blog within the
    myblog folder. Flask with python was used to run a virtual server on the
    PC

---
+++ my_blog/my_blog.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    return app.send_static_file('home.html')
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/contact')
+def contact():
+    return app.send_static_file('contact.html')
+
+@app.route('/post/1')
+def post1():
+    return 'This is post 1'
+
+@app.route('/post/<postnum>')
+def post(postnum):
+    return 'This is post' + postnum
